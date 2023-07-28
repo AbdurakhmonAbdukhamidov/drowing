d1ce32e2cc256f384742a728cf2898ac74ef51f9
Abdurahmon <abdurahmonmi6@gmail.com>
2023-07-28 13:13:52 -0700

    work crea element

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ var text= document.createElement('h1');
 document.body.append(text)
 var mousePos = null
 var mousewithoutpage = null
-var circle = document.querySelector('.circle');
+var circle2 = document.querySelector('.circle')
+var circle = null;
 var box = document.querySelector('.box')
 var check = false;
 var widthBox = window.innerWidth*10/100;
 console.log(widthBox);
 
+circle2.addEventListener('click', function(){
+    let subElement = document.createElement('div')
+    subElement.setAttribute('id','circle');
+    subElement.setAttribute('class','circle');
+    this.circle = document.querySelector('#circle');
+    box.append(subElement)
+})
+
 box.addEventListener('click', (event) => {
     console.dir(event);
+    console.log(circle);
     let topElement = parseInt(window.getComputedStyle(circle).top);
     let  leftElement = parseInt(window.getComputedStyle(circle).left)
     mousePos = { x: event.layerX, y: event.layerY};
