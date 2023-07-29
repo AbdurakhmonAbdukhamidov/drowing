1ed14b4f76afef1946e37d20378fa489cfdc8b2d
Abdurahmon <abdurahmonmi6@gmail.com>
2023-07-29 07:14:49 -0700

    optimazed

--- script.js
+++ script.js
@@ -3,47 +3,45 @@ document.body.append(text)
 var mousePos = null
 var mousewithoutpage = null
 var circle2 = document.querySelector('.circle')
-var circle = null;
 var box = document.querySelector('.box')
 var check = false;
+var shape;
 var widthBox = window.innerWidth*10/100;
-console.log(widthBox);
+var shapes = [];
+console.dir(Date.prototype.getMilliseconds);
 
 circle2.addEventListener('click', function(){
-    let subElement = document.createElement('div')
-    subElement.setAttribute('id','circle');
-    subElement.setAttribute('class','circle');
-    this.circle = document.querySelector('#circle');
-    box.append(subElement)
+    this.circle = document.createElement('div')
+    shapes.push(this.circle);
+    this.circle.setAttribute('id','circle'+(shapes.length));
+    this.circle.setAttribute('class','circle');
+    console.log(this.circle);
+    box.append(this.circle)
 })
 
-box.addEventListener('click', (event) => {
-    console.dir(event);
-    console.log(circle);
-    let topElement = parseInt(window.getComputedStyle(circle).top);
-    let  leftElement = parseInt(window.getComputedStyle(circle).left)
-    mousePos = { x: event.layerX, y: event.layerY};
-    mousewithoutpage ={ x: event.pageX, y: event.pageY} 
-    // console.log(`(${mousePos.x}, ${mousePos.y})`);
-    // console.log(topElement);
-    // console.log(leftElement);
+box.addEventListener('click', function(event){
+   if(event.target.attributes.class.value != "box"){
+      shape = document.querySelector('#'+event.target.attributes.id.value);
+      console.log(shape);
+      check == true;
+   }
     if(check == false){
-        if(mousewithoutpage.x >= leftElement && mousewithoutpage.x <= leftElement + parseInt(window.getComputedStyle(circle).width) || mousewithoutpage.y >= topElement && mousewithoutpage.y <= topElement + parseInt(window.getComputedStyle(circle).height) ){
             check = true;
-            circle.style.top = e.layerY + "px"
-            circle.style.left = e.layerX + "px"
-        }
+            shape.style.top = e.layerY + "px"
+            shape.style.left = e.layerX + "px"
     }else{
         check = false;
     }
+
+   
 
     
 });
 
 box.addEventListener('mousemove', (e) =>{
-    
     if(check == true){ 
-        circle.style.top = e.layerY + "px"
-        circle.style.left = e.layerX + "px"
+        console.log(shape);
+        shape.style.top = e.layerY + "px"
+        shape.style.left = e.layerX + "px"
     }
 })
